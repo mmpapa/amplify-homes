85c16d34dc39a97a6ed0f6418f22235d500cff0b
mmpapa <mmpapa9804@gmail.com>
2022-04-07 15:03:50 +0900

    認証外し２

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { HomeCollection, NavBar, MarketingFooter } from './ui-components'
-import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 
@@ -12,14 +11,6 @@ function App() {
         <MarketingFooter />
       </div>
   );
-}
-const styles = {
-  container: { width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 },
-  todo: {  marginBottom: 15 },
-  input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
-  todoName: { fontSize: 20, fontWeight: 'bold' },
-  todoDescription: { marginBottom: 0 },
-  button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
 }
 
 export default App;
