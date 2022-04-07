353ddf62cb9360890d4b7d1aebcc7a8be305a5f7
mmpapa <mmpapa9804@gmail.com>
2022-04-07 13:38:35 +0900

    ログイン追加

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 import { HomeCollection, NavBar, MarketingFooter } from './ui-components'
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <HomeCollection />
-      <MarketingFooter />
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+      <div className="App">
+        <NavBar />
+        <HomeCollection />
+        <MarketingFooter />
+        <button onClick={signOut}>Sign out</button>
+      </div>
+      )}
+    </Authenticator>
   );
 }
 
