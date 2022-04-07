b500392013cc982fc13a5c1837ef649f4b386ca9
mmpapa <mmpapa9804@gmail.com>
2022-04-07 14:40:16 +0900

    ログアウトボタンへスタイル設定とソーシャルログオン追加

--- src/App.js
+++ src/App.js
@@ -6,17 +6,25 @@ import '@aws-amplify/ui-react/styles.css';
 
 function App() {
   return (
-    <Authenticator>
+    <Authenticator socialProviders={['amazon', 'apple', 'facebook', 'google']}>
       {({ signOut, user }) => (
       <div className="App">
         <NavBar />
         <HomeCollection />
         <MarketingFooter />
-        <button onClick={signOut}>Sign out</button>
+        <button style={styles.button} onClick={signOut}>Sign out</button>
       </div>
       )}
     </Authenticator>
   );
+}
+const styles = {
+  container: { width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 },
+  todo: {  marginBottom: 15 },
+  input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
+  todoName: { fontSize: 20, fontWeight: 'bold' },
+  todoDescription: { marginBottom: 0 },
+  button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
 }
 
 export default App;
